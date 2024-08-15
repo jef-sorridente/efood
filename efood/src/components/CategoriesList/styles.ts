ebc63cbf6a7057f7cd5f9c5ac21cd1b1d40f6637
Jeferson Soares <jpadilha53@gmail.com>
2024-08-15 11:23:52 -0300

    refactor: remocao de arquivos utilizados somente na parte 1 do projeto.

--- efood/src/components/CategoriesList/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { Props } from ".";
-
-export const Container = styled.div<Omit<Props, "categories">>`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 48px 80px;
-  margin: 80px 0;
-`;
