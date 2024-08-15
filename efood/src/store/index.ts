ef2ceadd645db199dce92e3369a1555d1c5fdcc9
Jeferson Soares <jpadilha53@gmail.com>
2024-08-15 12:17:43 -0300

    feat: :sparkles: adicionado o reducer cart utilizando a api

--- efood/src/store/index.ts
+++ efood/src/store/index.ts
@@ -2,8 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import api from "../services/api";
 
+import cartReducer from "./reducers/cart";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+    [api.reducerPath]: api.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
